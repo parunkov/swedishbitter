934ec230af74f83106f3801fbbf02612e5fc14fc
parunkov <parunkov@gmail.com>
2021-03-12 13:14:38 +0500

    files added

---
+++ js/script.js
@@ -0,0 +1,1 @@
+var btnHeader=document.querySelector(".btn--header");btnHeader.addEventListener("click",function(e){e.preventDefault()});var btnHistory=document.querySelector(".btn--history");btnHistory.addEventListener("click",function(e){e.preventDefault()});
